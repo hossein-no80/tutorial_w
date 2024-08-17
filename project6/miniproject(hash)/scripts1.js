7c033392d03c23f2ec35fb29eefe9348948fbfdd
Hossein Rezaei <125386037+hossein-no80@users.noreply.github.com>
2024-08-17 16:35:29 +0330

    creat project6

---
+++ project6/miniproject(hash)/scripts1.js
@@ -0,0 +1,36 @@
+window.addEventListener("hashchange",(e)=> {
+  router()
+})
+
+function router() {
+  switch(location.hash) {
+    case"":
+      render({title:"Home", color:"red", href:"#home"})
+      break;
+    case"#home":
+      render({title:"Home", color:"red", href:"#home"})
+      break;
+    case"#search":
+      render({title:"SEARCH", color:"blue", href:"#search"})
+      break;
+    case"#likes":
+      render({title:"LIKES", color:"yellow", href:"#likes"})
+      break;
+    case"#profile":
+      render({title:"PROFILE", color:"pink", href:"#profile"})
+      break;
+  }
+}
+
+function render(data) {
+  document.querySelector("h1").innerHTML = data.title;
+  document.querySelector("main").style.backgroundColor = data.color;
+  document.querySelectorAll("a").forEach(element => {
+    if(element.href.includes(data.href)){
+      element.style.color = "black"
+    }else{
+      element.style.color = "#666"
+    }
+  })
+
+}
